cb929db2632622a4c55cad4f43b4eb8e8dc3aea6
ruoka <ruoka@users.noreply.github.com>
2015-07-01 01:07:20 +0300

    Looks good?

--- test/bson/model_test.cpp
+++ test/bson/model_test.cpp
@@ -1,89 +1,94 @@
-#include <iostream>
+#include <fstream>
 #include <gtest/gtest.h>
 #include "bson/model.hpp"
 
 using namespace std;
 using namespace std::literals::string_literals;
 
-TEST(BsonModelTest,Double)
+class BsonModelTest : public ::testing::Test
+{
+protected:
+
+    void SetUp()
+    {
+        ofs.open("test1.bson");
+    }
+
+    std::ofstream ofs;
+};
+
+TEST_F(BsonModelTest,Double)
 {
     bson::document d
     {
       {"Double"s, 12.55}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,String)
+TEST_F(BsonModelTest,String)
 {
     bson::document d
     {
       {"String"s, "B"s}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
 
-TEST(BsonModelTest,Array)
+TEST_F(BsonModelTest,Array)
 {
     bson::document d
     {
       {"Array"s, bson::array{"a"s, "b"s, "c"s, "d"s}}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,BooleanTrue)
+TEST_F(BsonModelTest,BooleanTrue)
 {
     bson::document d
     {
       {"Boolean"s, false}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,BooleanFalse)
+TEST_F(BsonModelTest,BooleanFalse)
 {
     bson::document d
     {
       {"Boolean"s, true}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,Integer32)
+TEST_F(BsonModelTest,Integer32)
 {
     bson::document d
     {
       {"Integer"s, 9}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,Long64)
+TEST_F(BsonModelTest,Long64)
 {
     bson::document d
     {
       {"Long"s, 2112ll}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,Mix)
+TEST_F(BsonModelTest,Mix)
 {
     bson::document d
     {
@@ -96,12 +101,11 @@ TEST(BsonModelTest,Mix)
       {"Integer"s, 9},
       {"Long"s, 2112ll}
     };
-    clog << d.size() << ": ";
-    clog.write(d.cbegin(), d.size());
-    clog << endl;
+    ofs.write(d.cbegin(), d.size());
+    ofs << endl;
 }
 
-TEST(BsonModelTest,Nested)
+TEST_F(BsonModelTest,Nested)
 {
     bson::document sons
     {
@@ -125,7 +129,6 @@ TEST(BsonModelTest,Nested)
       {"LuckyNumbers", bson::array{2,22,2112}}
     };
 
-    clog << papa.size() << ": ";
-    clog.write(papa.cbegin(), papa.size());
-    clog << endl;
+    ofs.write(papa.data(), papa.size());
+    ofs << endl;
 }
